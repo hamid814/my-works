41a5d831dcb826fef1f6fb07f450699c3c0a8fc7
hamid <hamid331994@gmail.com>
2020-01-05 13:32:54 +0330

    Updates

---
+++ precache-manifest.dcaf2f7643943b7a5c5c20808a88ffab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d238d4af88e54ca7221d26dca5a1611",
+    "url": "/my-works/index.html"
+  },
+  {
+    "revision": "6d5a52ad53db16586d5e",
+    "url": "/my-works/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98468c4f6a157d102540",
+    "url": "/my-works/static/js/2.b32ab416.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-works/static/js/2.b32ab416.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d5a52ad53db16586d5e",
+    "url": "/my-works/static/js/main.f9fa9e1a.chunk.js"
+  },
+  {
+    "revision": "061d137c210c7dbea819",
+    "url": "/my-works/static/js/runtime-main.232e5641.js"
+  }
+]);
